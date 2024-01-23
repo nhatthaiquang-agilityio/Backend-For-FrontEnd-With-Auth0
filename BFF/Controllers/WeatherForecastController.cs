d4c539cdbfd1d4400aa357f5bd23fd1d9d712fc0
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-01-23 12:10:44 +0700

    Add Client App

---
+++ BFF/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BackendForFrontend.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly Uri _apiEndpoint;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            IHttpClientFactory httpClientFactory,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+
+            if (configuration["WeatherApiEndpoint"] == null)
+                throw new ArgumentNullException("The Weather Api Endpoint is missing from the configuration");
+
+            _apiEndpoint = new Uri(configuration["WeatherApiEndpoint"], UriKind.Absolute);
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("Auth0", "access_token");
+
+            Console.WriteLine("accessToken");
+            Console.WriteLine(accessToken);
+
+            var httpClient = _httpClientFactory.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri(_apiEndpoint, "WeatherForecast"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var response = await httpClient.SendAsync(request);
+
+            Console.WriteLine("handle response");
+            Console.WriteLine(response);
+            response.EnsureSuccessStatusCode();
+
+            await response.Content.CopyToAsync(HttpContext.Response.Body);
+        }
+    }
+}
