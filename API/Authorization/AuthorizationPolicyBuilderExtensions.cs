70da497e67b1fe2960b4890bab4bf04e295cd19c
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-01-23 11:13:04 +0700

    Add Remote API

---
+++ API/Authorization/AuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Authorization
+{
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder builder, string scope)
+        {
+            return builder.AddRequirements(new ScopeRequirement(scope));
+        }
+    }
+}
---
+++ API/Authorization/ScopeRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Authorization
+{
+    public class ScopeRequirement : IAuthorizationRequirement
+    {
+        public string Scope { get; private set; }
+
+        public ScopeRequirement(string scope)
+        {
+            Scope = scope;
+        }
+    }
+}
